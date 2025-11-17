cd59ce1935acc70550386955eb0d014b354085bb
dhruvsahu611 <dsjee611@gmail.com>
2025-11-17 16:21:02 +0530

    loginForm, RegistrationForm and  JS intro

---
+++ JavaScript/basicJS/index.js
@@ -0,0 +1,20 @@
+var a=10;
+var a=20;
+let b=10;
+
+console.log(a,b);
+
+let ar = ["Abc", 12345, 45.67, 'a', true];
+
+console.log(ar);
+console.warn("Warning");
+console.error("This is an error");
+
+alert("This is alert");
+
+if(confirm("Are you sure")){
+    console.log("Thanks");
+}
+else{
+    console.error("Its ohk");
+}
