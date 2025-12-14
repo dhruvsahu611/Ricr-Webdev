a018c104583f66c16a5cbc6ed0bc4d2aa27cd725
dhruvsahu611 <dsjee611@gmail.com>
2025-12-14 15:04:46 +0530

    Jokes App

---
+++ JokesApp/script.js
@@ -0,0 +1,8 @@
+async function getNewJoke(){
+    const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
+
+    const data = await response.json();
+
+    document.getElementById("setup").innerText = data.setup;
+    document.getElementById("punchline").innerText = data.punchline;
+}
