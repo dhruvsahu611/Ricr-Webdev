3704e57735c0d3f6702e526b4ccee74191f6838a
dhruvsahu611 <dsjee611@gmail.com>
2025-12-25 21:34:29 +0530

    REACT Introduction

---
+++ REACT/Demo/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App(){
+  let a = 5;
+  let b = 10;
+  console.log(a+b);
+
+  return (
+    <>
+      <h1 id="head">Hello React World</h1>
+      <div>
+        This is Dhruv Sahu.
+      </div>
+    </>
+  );
+}
+
+
+export default App;
