c187705523340bf49b1593faaf59bd53fc71d662
dhruvsahu611 <dsjee611@gmail.com>
2026-01-24 17:58:29 +0530

    Portfolio Design with proper functioning and pending for more updation

---
+++ E-Cell task/Task4/codeFiles/script.js
@@ -0,0 +1,19 @@
+function connect(){
+    document.getElementById("linkedinContent").innerHTML=`<div>Thank you for your interest. We will contact you soon.</div>`;
+}
+
+function collegeChatbot(){
+    window.open('https://dhruvsahu611.github.io/project-1/');
+}
+
+function mapGame(){
+    window.open('https://maptaskricr.netlify.app/');
+}
+
+function ricrWeb(){
+    window.open('https://ricr-test-7dec-sunday.netlify.app/index.html');
+}
+
+function resume(){
+    window.open('https://drive.google.com/file/d/1E5kIwaeOizmkoYllBJbrQ0xKsMxv0cbz/view?usp=sharing');
+}
